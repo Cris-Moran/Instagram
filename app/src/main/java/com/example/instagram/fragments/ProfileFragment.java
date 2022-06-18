32487931d2a70d01b85a1e98298476ee6a51569e
Cris-Moran <68084820+Cris-Moran@users.noreply.github.com>
2022-06-17 17:04:55 -0700

    Successfully implemented feature that allows users to change their profile picture

--- app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
@@ -1,10 +1,14 @@
 package com.example.instagram.fragments;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -19,6 +23,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.instagram.LoginActivity;
@@ -30,10 +35,14 @@ import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.Scanner;
 
@@ -42,7 +51,8 @@ import java.util.Scanner;
  */
 public class ProfileFragment extends Fragment {
 
-    public static final int REQUEST_CODE = 1;
+    public static final int IMAGE_PICK_CODE = 1000;
+    public static final int PERMISSION_CODE = 1001;
     public static final String TAG = "ProfileFragment";
 
     private TextView tvProfileName;
@@ -93,37 +103,74 @@ public class ProfileFragment extends Fragment {
         btnChangePfp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(galleryIntent, REQUEST_CODE);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (getContext().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                        // permission not granted
+                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        requestPermissions(permissions, PERMISSION_CODE);
+                    } else {
+                        // permission already granted
+                        pickImageFromGallery();
+                    }
+                } else {
+                    // system os less than marshmellow
+                    pickImageFromGallery();
+                }
             }
         });
     }
 
+    private void pickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                    pickImageFromGallery();
+                } else {
+                    // permission was denied
+                    Toast.makeText(getContext(), "Permission was denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == REQUEST_CODE && null != data) {
-            if (resultCode == getActivity().RESULT_OK) {
-                Uri selectedImage = data.getData();
-                Log.i("selectedImage", "selectedImage: " + selectedImage.toString());
-                String[] filePathColumn = {MediaStore.Images.Media.DATA};
-                Cursor cursor = getActivity().getContentResolver().query(selectedImage,
-                        filePathColumn, null, null, null);
-                cursor.moveToFirst();
-                int columnIndex = cursor
-                        .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-                cursor.moveToFirst();
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == getActivity().RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            // set image
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
 
-                Log.i("columnIndex", "columnIndex: " + columnIndex);
+            Cursor cursor = getContext().getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            cursor.moveToFirst();
 
-                String picturePath = cursor.getString(columnIndex);
-                Log.i("picturePath", "picturePath: " + picturePath);
-                cursor.close();
-                File imgFile = new File("filename.txt");
-
-                ParseUser.getCurrentUser().put("profileImage", new ParseFile(imgFile));
-                Glide.with(getContext()).load(ParseUser.getCurrentUser().getParseFile("profileImage").getUrl()).into(ivPfp);
-            }
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String mCurrentPhotoPath = cursor.getString(columnIndex);
+            cursor.close();
+            File image = new File(mCurrentPhotoPath);
+            ParseFile pf = new ParseFile(image);
+            currentUser.put("profileImage", pf);
+            currentUser.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e != null) {
+                        Log.e(TAG, "Error while saving", e);
+                        Toast.makeText(getContext(), "Error while saving!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    Toast.makeText(getContext(), "Image saved successfully!", Toast.LENGTH_SHORT).show();
+                    Glide.with(getContext()).load(pf.getUrl()).into(ivPfp);
+                }
+            });
         }
     }
 
